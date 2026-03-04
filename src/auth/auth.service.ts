318e7195ab55c25453a00e479d387f634e435abc
Alex Karobia <akarobia30@gmail.com>
2026-03-04 18:18:42 +0300

    login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,9 +5,12 @@ import { AuthDto } from "./dto";
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async signIn(dto: AuthDto) {
+    // steps
+
+    // 1. find the user by email
     const user = await this.prisma.user.findUnique({
       where: {
         email: dto.email,
@@ -18,24 +21,26 @@ export class AuthService {
       throw new ForbiddenException("Credentials incorrect");
     }
 
+    // 2. compare the password with the hashed password in the database
     const isPasswordValid = await argon.verify(user.password, dto.password);
 
     if (!isPasswordValid) {
       throw new ForbiddenException("Credentials incorrect");
     }
 
+    const { password, ...userWithoutPassword } = user;
+
     return {
       message: "Signed in successfully",
-      user: {
-        id: user.id,
-        email: user.email,
-      },
+      user: userWithoutPassword,
     };
   }
 
   async signUp(dto: AuthDto) {
+    // 1. hashing the password
     const hashedPassword = await argon.hash(dto.password);
 
+    // 2. create and save the new user in the database
     try {
       const user = await this.prisma.user.create({
         data: {
@@ -44,12 +49,11 @@ export class AuthService {
         },
       });
 
+      const { password, ...userWithoutPassword } = user;
+
       return {
         message: "Signed up successfully",
-        user: {
-          id: user.id,
-          email: user.email,
-        },
+        user: userWithoutPassword
       };
     } catch {
       throw new ConflictException("Email already in use");
